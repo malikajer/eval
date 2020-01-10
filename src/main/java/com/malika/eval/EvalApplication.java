a1c20ac414ea1695477dd416ae6b8c00caea6913
malikajer <malikajerrar@gmail.com>
2020-01-10 10:08:49 +0100

    new fichiers

---
+++ src/main/java/com/malika/eval/EvalApplication.java
@@ -0,0 +1,13 @@
+package com.malika.eval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvalApplication.class, args);
+	}
+
+}
